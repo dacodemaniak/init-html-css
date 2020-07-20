c6411f56e77f70fb00a9b24bdbcb2fcc503260b0
Jean-Luc Aubert <jean-luc.a@ideafactory.fr>
2020-07-20 15:08:49 +0200

    Add babel, sass and config webpack

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,20 @@
+import * as $ from 'jquery'
+
 /**
  * @name App
  * @author Adrar - Juil. 2020
  * @version 1.0.0
  * @abstract Entry point of the current app
  */
+class App {
+    constructor() {
+        console.log('App is running !')
+    }
+}
+
+// App launcher
+$(document).ready(
+    () => {
+        new App() // Créer une instance de la classe App
+    }
+)
--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const webpack = require('webpack')
 // Charger les plugins nécessaires
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 // Main Webpack configuration
 module.exports = {
@@ -16,6 +17,29 @@ module.exports = {
     // Gestion des modules...
     module: {
         rules: [
+            // Conversion du code ES6 par Babel
+            {
+                test: /\.js$/,
+                exclude: '/node_modules',
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            // Prise en charge des fichiers scss, css...
+            {
+                test: /\.sc|ass$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader
+                    },
+                    {
+                        loader: 'css-loader'
+                    },
+                    {
+                        loader: 'sass-loader'
+                    }
+                ]
+            },
             {
                 test: /\.html$/i, // Expression régulière : tous les fichiers finissant par .html .HTML . Html
                 loader: 'html-loader'
@@ -42,6 +66,10 @@ module.exports = {
                 collapseWhitespace: true, // Compresse les espaces
                 removeEmptyAttributes: true
             }
+        }),
+        new MiniCssExtractPlugin({
+            filename: './src/css/[name].css',
+            chunkFilename: './src/css/[id].css'
         })
     ]
 }
